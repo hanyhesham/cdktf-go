6316f9865333fd351ebf7615548c06a068496e61
hanyhesham <hhesham.elsaeed@gmail.com>
2024-04-18 18:12:19 +0200

    add cdktf s3 creation

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
-	"github.com/hashicorp/terraform-cdk-go/cdktf"
-
-	"github.com/cdktf/cdktf-provider-aws-go/aws/v19/instance"
 	awsprovider "github.com/cdktf/cdktf-provider-aws-go/aws/v19/provider"
+	"github.com/cdktf/cdktf-provider-aws-go/aws/v19/s3bucket"
+	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
 func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
@@ -16,13 +15,12 @@ func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 		Region: jsii.String("us-west-1"),
 	})
 
-	instance := instance.NewInstance(stack, jsii.String("compute"), &instance.InstanceConfig{
-		Ami:          jsii.String("ami-01456a894f71116f2"),
-		InstanceType: jsii.String("t2.micro"),
+	s3bucket := s3bucket.NewS3Bucket(stack, jsii.String("bucket"), &s3bucket.S3BucketConfig{
+		Bucket: jsii.String("hany-test-terraform"),
 	})
 
-	cdktf.NewTerraformOutput(stack, jsii.String("public_ip"), &cdktf.TerraformOutputConfig{
-		Value: instance.PublicIp(),
+	cdktf.NewTerraformOutput(stack, jsii.String("name"), &cdktf.TerraformOutputConfig{
+		Value: s3bucket.Id(),
 	})
 
 	return stack
