1337646d48566aa10c82782a6ebfe6a2beb3a3c3
hanyhesham <hhesham.elsaeed@gmail.com>
2024-04-18 18:12:42 +0200

    add cdktf ec2 creation

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"cdk.tf/go/stack/generated/hashicorp/aws/instance"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 	awsprovider "github.com/cdktf/cdktf-provider-aws-go/aws/v19/provider"
@@ -15,12 +16,21 @@ func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 		Region: jsii.String("us-west-1"),
 	})
 
+	instance := instance.NewInstance(stack, jsii.String("compute"), &instance.InstanceConfig{
+		Ami:          jsii.String("ami-01456a894f71116f2"),
+		InstanceType: jsii.String("t2.micro"),
+	})
+
 	s3bucket := s3bucket.NewS3Bucket(stack, jsii.String("bucket"), &s3bucket.S3BucketConfig{
 		Bucket: jsii.String("hany-test-terraform"),
 	})
 
 	cdktf.NewTerraformOutput(stack, jsii.String("name"), &cdktf.TerraformOutputConfig{
 		Value: s3bucket.Id(),
+	})
+
+	cdktf.NewTerraformOutput(stack, jsii.String("public_ip"), &cdktf.TerraformOutputConfig{
+		Value: instance.PublicIp(),
 	})
 
 	return stack
